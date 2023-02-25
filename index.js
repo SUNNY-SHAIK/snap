fc74f64bc026e530efe64c05d4481a7468539cac
SUNNY-SHAIK <116821664+SUNNY-SHAIK@users.noreply.github.com>
2023-02-25 08:53:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express= require("express")
+const app= express()
+const cors=require("cors")
+const bodyparser=require("body-parser")
+const port= 4000
+const studentDetails={name:"sunny ",age:19,gender:"male"}
+const produtsDetails=require("./product")
+const mongoose=require("mongoose")
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.use(cors())
+app.use(bodyparser.urlencoded({
+	extended:true
+}))
+
+app.use(bodyparser.json())
+mongoose.connect("mongodb+srv://sunnyshaik637:sunnyshaik@cluster0.kmcdjnu.mongodb.net/?retryWrites=true&w=majority")
+	.then(()=>{
+		console.log("database connected successfully")
+	})
+	app.post("/add",(req,res)=>{
+		console.log(req.body)
+		const {username,password}=req.body
+		const newValue=new Content({
+			username:username,
+			password:password
+		})
+		newValue.save()
+	})
+	app.get("/users",(req,res)=>{
+		return Content.find()
+				.then((values)=>res.json(values))
+	})
+
+app.get("/",(req,res)=>{
+	res.send("it is")
+})
+
+
+app.get("/student",(req,res)=>{
+	res.json(studentDetails)
+})
+
+app.get("/products",(req,res)=>{ 
+	res.json(produtsDetails)
+})
+
+
+
+
+app.listen(port,()=>console.log("started"))
+	
